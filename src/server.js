d4432f7ef0b413fe1d1b3020f26b8866959fe3d7
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2021-06-02 18:11:21 +0700

    main program

--- src/server.js
+++ src/server.js
@@ -6,11 +6,8 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 
-      process.env.NODE_ENV === 'development' 
-      ? 'localhost' 
-      : 'javascript-bookshelfapi.herokuapp.com',
+    port: process.env.PORT || 3000,
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
