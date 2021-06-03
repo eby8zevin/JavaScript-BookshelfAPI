2c99130fd7d2e64021cb1698ec1eab1643d39dcd
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-03 23:10:31 +0700

    main program

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 8080,
-    host: 'localhost',
+    //host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
